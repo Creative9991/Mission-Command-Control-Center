db725b133a8c2eecb8ba34d18d8c8f8151510802
Mukesh <mukeshreddykatipally@gmail.com>
2020-12-20 17:23:02 -0700

    added satelitte page

--- src/components/Agency/agency.js
+++ src/components/Agency/agency.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import '../../styles/agency.scss';
 import {AgencyListId} from "../../services/agencyAPI";
-
-
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import { Link} from 'react-router-dom';
+import { Card } from 'antd';
 
 
 export default class Agency extends React.Component {
@@ -10,38 +11,44 @@ export default class Agency extends React.Component {
         super(props)
 
         this.state = {
-            agenciesId: {},
+            agenciesId : {},
             loading: true,
             error: false,
+            currentId : null,
+            launches : []
         };
+
+        
     }
-  
- 
-  
 
     async componentDidMount() {
         try {
-            const agenciesId = await AgencyListId(this.props.match.params.id);
-            this.setState({ agenciesId , loading: false });
+            console.log("Id is" , this.props.match.params.id);
+            let agenciesId = await AgencyListId(this.props.match.params.id);
+            console.log(agenciesId);
+            this.setState({ agenciesId, currentId : true, loading: false });
         } catch (err) {
             console.log('outside catch', err);
         }
     }
 
+
+
     render(){
-        
-       
         //const {error, loading, agenciesId } = this.state;
       return(
   <div className="space-agency-details">
-      <h1 className="space-header-paragraph"><p className="space-agency-detials-header"><strong>{this.state.agenciesId.imgName}</strong></p> </h1>
-          <div className="movie-details-poster-wrapper">
+      <h1 className="space-header-paragraph">
+          <span className='agency-arrow-left'><Link to="/space_agencies"><ArrowLeftOutlined /></Link> </span>
+     
+          <p className="space-agency-detials-header"><strong>{this.state.agenciesId.imgName}</strong></p> </h1>
+          <div className="agency-details-poster-wrapper">
               <img
-                  className="movie-details-poster"
-                  alt="movie poster"
+                  className="agency-details-poster"
+                  alt="agency poster"
               />
-                <div className="movie-details-info">
-                <div className="movie-details-info__overview">
+                <div className="agency-details-info">
+                <div className="agency-details-info__overview">
                   <strong>Agency Overview  : </strong>
                   <strong> {this.state.agenciesId.imgName}</strong><br/>
                   <strong>Year Established  : </strong>
@@ -55,6 +62,33 @@ export default class Agency extends React.Component {
               </div>
           </div>
           </div>
+
+          <Card>
+          <h1>List of main launches from the : {this.state.agenciesId.imgName}</h1>
+          <table id="customers">
+                     <tbody>
+                    <tr >
+                     <th>Mission ID</th>
+                     <th>Mission Name</th>
+                     <th>Launch Year</th>
+                     <th >Mission Status</th>
+                    </tr>
+                    { 
+      //console.log(this.state.agenciesId.launches)
+      typeof this.state.agenciesId.launches !== 'undefined' && this.state.agenciesId.launches.map((launch , mission_launches) =>{
+          return(
+                    <tr key= {mission_launches}>
+                    <td>{launch.mission_id}</td>
+                    <td>{launch.mission}</td>
+                     <td>{launch.launch_year}</td>
+                     <td>{launch.status}</td>
+                    </tr>
+                  )})
+    }
+              </tbody>
+                </table> 
+                </Card>
+      
           </div>
       )
     }
